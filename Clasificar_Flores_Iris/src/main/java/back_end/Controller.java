3cdc8c24952e00a52f1e4699c9e53ca79d68f95c
Angel Hernandez <LuXuzStudios@gmail.com>
2025-01-28 14:47:44 -0500

    feat: Agregate Controller and Validator

---
+++ Clasificar_Flores_Iris/src/main/java/back_end/Controller.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package back_end;
+
+import neuralNetwork.NeuralNetwork;
+
+/**
+ *
+ * @author Angel Hernandez
+ */
+public class Controller {
+    private NeuralNetwork neuralNetwork;
+    private Validator validator;
+    
+    public void recieveInput(String widthLeaf, String lengthLeaf, String widthStem, String lengthStem)
+    {
+        
+    }
+}
---
+++ Clasificar_Flores_Iris/src/main/java/back_end/Validator.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package back_end;
+
+/**
+ *
+ * @author Angel Hernandez
+ */
+public class Validator {
+    
+    private String password;
+    
+    public Validator(String password)
+    {
+        this.password = password;
+    }
+    
+    public boolean isCorrectInput(String input)
+    {
+        boolean correct=true;
+        if(input.trim().isEmpty() || isLetter(input))
+            correct=false;
+        return correct;
+    }
+    
+    
+    private boolean isLetter(String input)
+    {
+        boolean letter = true;
+        char[] array = input.toCharArray();
+        for(int i=0;i<input.length();i++)
+        {
+            if(Character.isDigit(array[i]))
+                letter=false;
+        }
+        return letter;
+    }
+    
+    
+    private boolean isNumber(String input)
+    {
+        boolean number = true;
+        char[] array = input.toCharArray();
+        for(int i=0;i<input.length();i++)
+        {
+            if(Character.isAlphabetic(array[i]))
+                number=false;
+        }
+        return number;
+    }
+    
+    
+    public boolean checkPasswor(String input)
+    {
+        boolean isCorrect = true;
+        if(!input.equals(password))
+            isCorrect=false;
+        return isCorrect;
+    }
+    
+}
