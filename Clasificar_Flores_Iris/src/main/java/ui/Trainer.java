c309d58ca5dccb82a924b54a511a15de1f8e6037
kurama012299 <kristian04aguila@gmail.com>
2025-02-17 02:52:35 -0500

    feat(Trainer):Agregar panel informacion

--- Clasificar_Flores_Iris/src/main/java/ui/Trainer.java
+++ Clasificar_Flores_Iris/src/main/java/ui/Trainer.java
@@ -48,17 +48,11 @@ public class Trainer extends javax.swing.JFrame {
         getRootPane().putClientProperty(FlatClientProperties.TITLE_BAR_SHOW_ICON,true);
         getRootPane().putClientProperty(FlatClientProperties.TITLE_BAR_SHOW_ICONIFFY,true);
         UIControllers.design();
-        JPanel blankPanel1 = new JPanel();
-        JPanel blankPanel2 = new JPanel(new BorderLayout());
-        JTable table=new JTable();
-        table.setVisible(true);
-        blankPanel1.setBackground(Color.white);
-        blankPanel2.setBackground(Color.white);
-        JPanel panelGrafico = crearPanelGrafico();
-        cardPanel.add(panelGrafico, "Grafico 1");
-        cardPanel.add(blankPanel1);
-        cardPanel.add(blankPanel2);
         
+        
+        
+        //JPanel panelGrafico = crearPanelGrafico();
+        //cardPanel.add(panelGrafico, "Grafico 1");
     }
     
     
@@ -122,6 +116,18 @@ public class Trainer extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        paneMetricas = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        labelIndicationMetricas = new javax.swing.JLabel();
+        paneLabels = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
         cardPanel = new javax.swing.JPanel();
         buttonPanel = new javax.swing.JPanel();
         buttonGraph2 = new javax.swing.JButton();
@@ -138,6 +144,72 @@ public class Trainer extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        paneMetricas.setBackground(new java.awt.Color(255, 255, 255));
+        paneMetricas.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        paneMetricas.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(32, 70, 440, 190));
+
+        labelIndicationMetricas.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        labelIndicationMetricas.setForeground(new java.awt.Color(102, 153, 255));
+        labelIndicationMetricas.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        labelIndicationMetricas.setText("Datos Metricas");
+        paneMetricas.add(labelIndicationMetricas, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 30, 300, 30));
+
+        jPanel1.add(paneMetricas, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 520, -1));
+
+        paneLabels.setBackground(new java.awt.Color(255, 255, 255));
+        paneLabels.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(102, 153, 255));
+        jLabel1.setText("Precision Total: ");
+        paneLabels.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 60, 130, 20));
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(102, 153, 255));
+        jLabel2.setText("Setosa: ");
+        paneLabels.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 90, 130, 20));
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(102, 153, 255));
+        jLabel4.setText("Error:");
+        paneLabels.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, 130, 20));
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(102, 153, 255));
+        jLabel5.setText("Epocas:");
+        paneLabels.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, 130, 20));
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(102, 153, 255));
+        jLabel6.setText("Versicolor:");
+        paneLabels.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 190, 130, 20));
+
+        jLabel7.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(102, 153, 255));
+        jLabel7.setText("Virginica:");
+        paneLabels.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 140, 130, 20));
+
+        jLabel9.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(102, 153, 255));
+        jLabel9.setText("Precision por clase :");
+        paneLabels.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 50, 130, 20));
+
+        jPanel1.add(paneLabels, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 510, 250));
 
         cardPanel.setBackground(new java.awt.Color(255, 255, 255));
         cardPanel.setLayout(new java.awt.CardLayout());
@@ -168,7 +240,7 @@ public class Trainer extends javax.swing.JFrame {
 
         buttonGraph4.setBackground(new java.awt.Color(255, 255, 255));
         buttonGraph4.setForeground(new java.awt.Color(0, 0, 0));
-        buttonGraph4.setText("Ver mas");
+        buttonGraph4.setText("Informacion");
         buttonGraph4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 buttonGraph4ActionPerformed(evt);
@@ -193,18 +265,24 @@ public class Trainer extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void buttonGraph2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonGraph2ActionPerformed
+        cardPanel.setVisible(true);
+        paneMetricas.setVisible(false);
+        paneLabels.setVisible(false);
        CardLayout c1= (CardLayout)(cardPanel.getLayout());
-        c1.show(cardPanel, "Grafico 1");
+       c1.show(cardPanel, "Grafico 1");
     }//GEN-LAST:event_buttonGraph2ActionPerformed
 
     private void buttonGraph3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonGraph3ActionPerformed
-        CardLayout c2= (CardLayout)(cardPanel.getLayout());
-        c2.show(cardPanel, "");
+        
+        cardPanel.setVisible(false);
+        paneMetricas.setVisible(true);
+        paneLabels.setVisible(false);
     }//GEN-LAST:event_buttonGraph3ActionPerformed
 
     private void buttonGraph4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonGraph4ActionPerformed
-        CardLayout c3= (CardLayout)(cardPanel.getLayout());
-        c3.show(cardPanel,"");
+        cardPanel.setVisible(false);
+        paneMetricas.setVisible(false);
+        paneLabels.setVisible(true);
     }//GEN-LAST:event_buttonGraph4ActionPerformed
 
     private void buttonBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonBackActionPerformed
@@ -228,6 +306,18 @@ public static void main(String args[]) {
     private javax.swing.JButton buttonGraph4;
     private javax.swing.JPanel buttonPanel;
     private javax.swing.JPanel cardPanel;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JLabel labelIndicationMetricas;
+    private javax.swing.JPanel paneLabels;
+    private javax.swing.JPanel paneMetricas;
     // End of variables declaration//GEN-END:variables
 }
